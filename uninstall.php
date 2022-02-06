39b82596535c819cfd5fb9f20b89b91b96ff46c7
Vince Pelss <vpelss@users.noreply.github.com>
2022-02-06 09:44:31 -0500

    added

---
+++ uninstall.php
@@ -0,0 +1,19 @@
+<?php
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit($staus='WP_UNINSTALL_PLUGIN not defn');
+	//die('WP_UNINSTALL_PLUGIN not defn');
+}
+
+/*
+		$books = get_posts( array('book_type' => 'book' , 'numberposts' => -1) );
+		foreach($books as $book){
+			wp_delete_post( $book->ID , true );
+		}
+*/
+		do_action( 'qm/debug', 'That happened!' );
+
+		global $wpdb;
+		$wpdb->query("DELETE FROM wp_posts WHERE post_type = 'book'");
+		$wpdb->query("DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT ID FROM wp_posts)");
+		$wpdb->query("DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT ID FROM wp_posts)");
